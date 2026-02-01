7dc62366d40e18729ae5a1bbab0d5054cbcc58f3
ChloeAmelinda <trangthichngunuong@gmail.com>
2026-02-01 22:00:13 +0700

    Delete Session5 directory

--- Session5/HW3/main.java
+++
@@ -1,9 +0,0 @@
-package Session5.HW3;
-
-public class main {
-    static void main(String[] args) {
-        bai3.Product p = new bai3.Product("B12","candy",1200);
-        p.display();
-        p.setprice(1200);
-    }
-}
